b6d3f8a7be76a75f655a8bae2023eda56087b54d
Benoît Fleury <me@benfle.com>
2018-12-08 07:32:12 -0500

    cleanup

--- day8.clj
+++ day8.clj
@@ -7,31 +7,30 @@
 
 (def test-license [2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2])
 
-(declare read-node)
-
 (defn read-license
   [license]
-  (let [v (volatile! license)
-        read-n (fn [n] (let [res (take n @v)] (vswap! v #(drop n %)) res))
-        read-n-node (fn read-n-node [n]
+  (let [;; For clarity we will use local state to read the license
+        v (volatile! license)
+        ;; Fn to consume n elements from the license
+        read! (fn [n] (let [elts (take n @v)] (vswap! v #(drop n %)) elts))
+        ;; Fn to consume n nodes from the license
+        read-nodes! (fn read-nodes! [n]
                       (when-not (zero? n)
-                        (cons (let [[n-children n-metadata] (read-n 2)
-                                    children (read-n-node n-children)
-                                    metadata (read-n n-metadata)]
-                                (cond-> {}
-                                  (seq children) (assoc :children children)
-                                  (seq metadata) (assoc :metadata metadata)))
-                              (read-n-node (dec n)))))]
-    (first (read-n-node 1))))
+                        (cons (let [[n-children n-metadata] (read! 2)]
+                                {:children (read-nodes! n-children)
+                                 :metadata (read! n-metadata)})
+                              (read-nodes! (dec n)))))]
+    (first (read-nodes! 1))))
 
 (defn check-license
+  "Check the license with the given check function."
   [license check-node]
   (check-node (read-license license)))
 
 ;; Part One
 
 (defn check1
-  [{:keys [metadata children]}]
+  [{:keys [children metadata]}]
   (reduce +
           (reduce + 0 metadata)
           (map check1 children)))
@@ -43,7 +42,7 @@
 ;; Part Two
 
 (defn check2
-  [{:keys [metadata children]}]
+  [{:keys [children metadata]}]
   (if (seq children)
     (reduce + 0 (map #(check2 (nth children (dec %) 0)) metadata))
     (reduce + 0 metadata)))
